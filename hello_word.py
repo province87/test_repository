f248e720304d3c6c228683ad80a80fe5041f71b8
lsprovince <63831130+lsprovince@users.noreply.github.com>
2020-04-17 11:23:54 +0800

    Update hello_word.py
    update

--- hello_word.py
+++ hello_word.py
@@ -1,11 +1,6 @@
-ddd
 #!/usr/bin/env python3
 #coding=utf-8
 print("hello word！")
-print("hello")
-print("nihao"
-print("nihao"))
-print("niddddhao"))
 list_1=[2,1,3,4,5]
 for i in list_1:
     print(i)
